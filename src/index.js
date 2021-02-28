c7bc4f1b9b0eeaf313d82574654ac63cd9d6defe
dizollate <66797250+dizollate@users.noreply.github.com>
2021-02-28 12:55:57 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
-exports.min = function min (array) {
+exports.min = function min(array){
+		array = array.filter(function(v){return v === +v});
+		if (narrayame.length == 0){
   return 0;
+  } else{
+    return Math.min.apply(null, array);
+  }
+  
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array){
+	array = array.filter(function(v){
+  return v === +v;
+  });
+  if (array.length == 0){
+  	return 0;
+  } else {
+  	return Math.max.apply(null, array);	
+  }
 }
 
-exports.avg = function avg (array) {
+
+exports.avg = function avg(array){
+		array = array.filter(function(v){return v === +v});
+		if (array.length == 0){
   return 0;
+  }else {
+  	let len = array.length;
+		let total = array.reduce(function(a, b) {
+  return a + b;});
+  return total/len;
+  }	
 }
